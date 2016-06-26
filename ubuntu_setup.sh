d32a28617205571e9b423e66cb17e605417a8c24
Jon Craton <jncraton@gmail.com>
2016-06-26 19:32:42 -0400

    Added .bashrc

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -80,7 +80,7 @@ xinput --set-prop "Razer Razer Diamondback Optical Mouse" "Device Accel Velocity
 xinput --set-prop "Razer Razer Diamondback Optical Mouse" "Device Accel Profile" -1
 
 # Install my favorites
-yes | sudo apt-get install wmctrl scite vlc p7zip-full nautilus-dropbox git
+yes | sudo apt-get install wmctrl scite vlc p7zip-full nautilus-dropbox git ffmpeg
 
 # Veracrypt deps
 yes | sudo apt-get install dmsetup
@@ -97,3 +97,23 @@ git config --global push.default simple
 
 # Finish Dropbox install (GUI interaction required)
 dropbox start -i
+
+# Build ffmpeg
+sudo apt-get -y install autoconf automake build-essential libass-dev libfreetype6-dev \
+  libsdl1.2-dev libtheora-dev libtool libva-dev libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev \
+  libxcb-xfixes0-dev pkg-config texinfo zlib1g-dev yasm libmp3lame-dev libx264-dev libx265-dev && \
+cd /tmp && \
+wget -O fdk-aac.tar.gz https://github.com/mstorsjo/fdk-aac/tarball/master && \
+tar xzvf fdk-aac.tar.gz && \
+cd mstorsjo-fdk-aac* && \
+autoreconf -fiv && \
+./configure --disable-shared && \
+sudo make && sudo make install && sudo make distclean && \
+sudo cp /usr/local/lib/* /usr/lib && \
+git clone git://source.ffmpeg.org/ffmpeg.git && \
+cd ffmpeg && \
+./configure --enable-gpl --enable-libx264 --enable-libx265 --enable-libmp3lame --enable-nonfree --enable-libfdk-aac && \
+sudo make && \
+sudo make install
+
+
